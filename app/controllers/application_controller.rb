8ffc44ba4ac5cf8e898612b31052a4e19d13c62a
Arvind Tiwari <artiwari@cb.carrerbuilder.com>
2020-02-03 12:07:11 +0530

    Add authentication for user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  #before_action :user_is_logged_in
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def authenticate
+    unless current_user.present?
+  	  redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate,:except => [:new,:create, :destroy]
   def new
     @user = User.new
   end
